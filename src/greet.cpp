63a549997eb49ec6c739f54957e496c29726f5c4
Jacob Hummer <jcbhmr@outlook.com>
2024-04-12 14:26:29 -0500

    save

--- src/greet.cpp
+++ src/greet.cpp
@@ -1,9 +1,12 @@
 #include <iostream>
 #include <string>
 #include "greet.hpp"
+#include "math.hpp"
 
 std::string greeting(const std::string& name) {
-    return "Hello " + name + "!";
+    int n = 5;
+    int res = fib(n);
+    return "Hello " + name + "! Did you know that fib(" + std::to_string(n) + ") is " + std::to_string(res) + "?";
 }
 
 void greet(const std::string& name) {
