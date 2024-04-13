f024b3a47c10a3bc6dd328c8e8748ff0ccbc395f
Jacob Hummer <jcbhmr@outlook.com>
2024-04-13 15:31:06 -0500

    save

--- greet/greet_cgo.go
+++
@@ -1,8 +0,0 @@
-package greet
-
-import (
-	_ "github.com/jcbhmr/go-hello-swig/greet/deps/fmt"
-	_ "github.com/jcbhmr/go-hello-swig/greet/src"
-)
-
-//go:generate swig -go -intgosize 32 -c++ -Iinclude -Ideps/fmt/include greet.i
--- greet/src/greet.cpp
+++ greet/src/greet.cpp
@@ -1,14 +1,15 @@
 #include <iostream>
 #include <string>
+#include <fmt/format.h>
 #include "greet.hpp"
 #include "math.hpp"
 
 std::string greeting(const std::string& name) {
     int n = 5;
     int res = fib(n);
-    return "Hello " + name + "! Did you know that fib(" + std::to_string(n) + ") is " + std::to_string(res) + "?";
+    return fmt::format("Hello {}! Did you know that fib({}) is {}?", name, n, res);
 }
 
 void greet(const std::string& name) {
-    std::cout << greeting(name) << "\n";
+    fmt::print("{}\n", greeting(name));
 }
